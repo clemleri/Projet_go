8b63732fc5347ade366d56b29a83cba8c95af080
Clemlery <lerouleyclement@gmail.com>
2023-11-02 17:18:36 +0100

    test_size content

--- floor/update_test.go
+++ floor/update_test.go
@@ -5,8 +5,20 @@ import (
 	"reflect"
 )
 
-func Test_file(t *testing.T) {
-	content := 
+func Test_size(t *testing.T) {
+	content := updateFromFileFloor(camXPos, camYPos) 
+	if len(content)!= Global.NumTileY {
+		t.Fail()
+	}
+	for i:=0;i<len(content);i++ {
+		if content[i]!= Global.NumTileX {
+			t.Fail()
+		}
+	}
 }
 
+func Test_out(t *testing.T) {
+	content := updateFromFileFloor(camXPos, camYPos) 
+	for i:=0; i<len()
+}
 
